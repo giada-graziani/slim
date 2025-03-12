1b19380b9c90a66a276ab209331c31080bca2d18
lai2-24 <lai2-24@itismeucci.com>
2025-03-12 10:40:22 +0100

    commit1

---
+++ controllers/AlunniController.php
@@ -0,0 +1,70 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class AlunniController
+{
+  public function index(Request $request, Response $response, $args){
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $result = $mysqli_connection->query("SELECT * FROM alunni");
+    $results = $result->fetch_all(MYSQLI_ASSOC);
+
+    $response->getBody()->write(json_encode($results));
+    return $response->withHeader("Content-type", "application/json")->withStatus(200);
+  }
+
+  public function view(Request $request, Response $response, $args){
+    $id = $args['id'];
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $result = $mysqli_connection->query("SELECT * FROM alunni WHERE id='$id'");
+    $results = $result->fetch_all(MYSQLI_ASSOC);//scorre l'array per visualizzare tutti gli alunni
+    
+    if($results){
+      $response->getBody()->write(json_encode($results));
+    }
+    else{
+      $response->getBody()->write("{'messaggio':'not found'}");
+    }
+      return $response->withHeader("Content-type", "application/json")->withStatus(200);
+  }
+
+  /*curl -X POST localhost:8080/alunni --data '{"nome":"giada","cognome":"graziani"}' -H "Content-Type:application/json"*/
+  public function create(Request $request, Response $response, $args){
+
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $body=json_decode($request->getbody()->getContents(), true);
+    $nome= $body["nome"];
+    $cognome=$body["cognome"];
+
+    $result = $mysqli_connection->query("INSERT INTO alunni(nome,cognome) VALUES('$nome','$cognome')");
+
+    if($result){
+      $response->getBody()->write("{'status':'ok'}");
+    }
+    else{
+      $response->getBody()->write("{'status':'not found'}");
+    }
+
+    return $response->withHeader("Content-type", "application/json")->withStatus(200);
+  }
+  /*curl -X PUT localhost:8080/alunni/2 --data '{"nome":"gaia","cognome":"bottai"}' -H "Content-Type:application/json"*/
+  public function update(Request $request, Response $response, $args){
+    $id = $args['id'];
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $body=json_decode($request->getbody()->getContents(), true);
+    $nome= $body["nome"];
+    $cognome=$body["cognome"];
+
+    $result = $mysqli_connection->query("UPDATE alunni SET nome='$nome',cognome='$cognome' WHERE id=$id");
+
+    if($result){
+      $response->getBody()->write("{'status':'ok'}");
+    }
+    else{
+      $response->getBody()->write("{'status':'404 not found'}");
+    }
+
+    return $response->withHeader("Content-type", "application/json")->withStatus(200);
+  }
+
+}
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+//DEFINISCE LE ROTTE DA INTERCETTARE
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/controllers/HelloController.php';
+require __DIR__ . '/controllers/AlunniController.php';
+
+$app = AppFactory::create();
+
+$app->get('/hello', "HelloController:hello"); //rimanda alla funzione della classe hellocontroller
+$app->get('/hello/{name}',"HelloController:hello_with_name");
+$app->get('/json/{name}', "HelloController:json_name");
+
+
+$app->get('/alunni', "AlunniController:index");
+$app->get('/alunni/{id}', "AlunniController:view");
+$app->post('/alunni', "AlunniController:create");
+$app->put('/alunni/{id}', "AlunniController:update");
+$app->delete('/alunni/{id}', "AlunniController:destroy");
+$app->run();
